ae1422a68d343cf688eb4f420871905d6f00cace
Will Brammer <williamedwardbrammer@gmail.com>
2024-03-12 17:47:40 +0900

    style: add styling to contact form

--- my-website/src/components/StickyContactButton.jsx
+++ my-website/src/components/StickyContactButton.jsx
@@ -20,32 +20,45 @@ const StickyContactButton = () => {
           </button>
         </div>
       ) : (
-        <div className="absolute top-0 right-0 h-screen w-screen z-50 bg-sky-100 flex flex-col items-center justify-center">
-          <div className="fixed top-5 right-5">
-            <button
-              onClick={modalOfContact}
-              className="bg-blue-600 text-black rounded-full p-4 hover:bg-pink-600 focus:outline-none focus:ring focus:border-blue-100 transform transition-transform duration-300 hover:rotate-180 hover:scale-110"
-              role="img"
-              aria-label="Close"
+        <>
+          <div className="contact-fill absolute top-0 right-0 h-screen w-screen z-50 bg-sky-100 flex flex-col items-center justify-center">
+            <div className="fixed top-5 right-5">
+              <button
+                onClick={modalOfContact}
+                className="bg-blue-600 text-black rounded-full p-4 hover:bg-pink-600 focus:outline-none focus:ring focus:border-blue-100 transform transition-transform duration-300 hover:rotate-180 hover:scale-110"
+                role="img"
+                aria-label="Close"
+              >
+                <XCircleIcon />
+              </button>
+            </div>
+            <a
+              href="https://github.com/NabbeunNabi"
+              className="button-fill-l-to-r flex items-center justify-center h-2/5 w-4/5 rounded-full"
+              target="_blank"
             >
-              <XCircleIcon />
-            </button>
-          </div>
-          <div className="my-4">
-            <a href="https://github.com/NabbeunNabi">
-              <img src="/github-mark.png" alt="the github logo" />
-            </a>
-          </div>
-          <div cl>
-            <a href="https://www.linkedin.com/in/william-brammer/">
               <img
-                src="/linkedin-black.png"
-                alt="the linkedIn logo"
-                className="h-1/5"
+                src="/github.svg"
+                alt="the github logo"
+                className="h-2/5 mr-4"
               />
+              <span className="text-4xl">Github</span>
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/william-brammer/"
+              className="button-fill-l-to-r flex items-center justify-center h-2/5 w-4/5 rounded-full"
+              target="_blank"
+            >
+              <img
+                src="/linkedin.svg"
+                alt="the linkedIn logo"
+                className="h-2/5 mr-4"
+              />
+              <span className="text-4xl">LinkedIn</span>
             </a>
           </div>
-        </div>
+        </>
       )}
     </>
   );
