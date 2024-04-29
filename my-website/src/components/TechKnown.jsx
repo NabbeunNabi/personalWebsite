2ef5bbc4bf6d2821041ba1d8aa06a09e65b70c25
William Brammer <williamedwardbrammer@gmail.com>
2024-04-30 07:55:06 +0900

    feat: implement tech skills section

--- my-website/src/components/TechKnown.jsx
+++ my-website/src/components/TechKnown.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import FlipCard from "./flipcard/FlipCard";
 import data from "../../text.json";
+import StaticCardForTech from "./flipcard/static-card/StaticCard";
 
 const TechKnown = () => {
   return (
-    <div className="flex flex-col border-4 rounded-lg shadow-lg shadow-gray-500 p-2 m-4 w-4/5 justify-center">
+    <div className="flex flex-col border-4 rounded-lg shadow-lg shadow-gray-500 p-2 m-4 w-4/5 justify-center items-center">
       <h1 className="font-bold text-3xl underline mb-2">Tech Skills</h1>
+      <StaticCardForTech
+        typeOfTech="Languages"
+        arrayOfTechImages={Object.values(data.techPics.Languages)}
+      />
+      <StaticCardForTech
+        typeOfTech="Frontend"
+        arrayOfTechImages={Object.values(data.techPics.Frontend)}
+      />
+      <StaticCardForTech
+        typeOfTech="Backend and Databases"
+        arrayOfTechImages={Object.values(data.techPics.BackendAndDB)}
+      />
+      <StaticCardForTech
+        typeOfTech="Testing and Deployment"
+        arrayOfTechImages={Object.values(data.techPics.TestingAndDeployment)}
+      />
     </div>
   );
 };
---
+++ my-website/src/components/flipcard/static-card/StaticCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const StaticCardForTech = ({ typeOfTech, arrayOfTechImages }) => {
+  console.log(arrayOfTechImages);
+  return (
+    <>
+      <div className="border-4 rounded-lg shadow-lg shadow-gray-500 p-2 m-4 w-4/5 ">
+        <h1 className="underline text-xl font-semibold p-1 mb-2">
+          {typeOfTech}
+        </h1>
+        <div className="flex flex-wrap justify-around">
+          {arrayOfTechImages?.map((imageSource) => {
+            return (
+              <img
+                className="img-tech-zoom p-2"
+                src={imageSource}
+                height={70}
+                width={70}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StaticCardForTech;
