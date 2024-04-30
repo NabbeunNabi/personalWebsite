220d99bb793cb58a806c092f7feba43d1ec1c8f9
William Brammer <williamedwardbrammer@gmail.com>
2024-04-30 10:53:49 +0900

    feat: update based on feedback

--- my-website/src/components/VideoWithOverlay.jsx
+++ my-website/src/components/VideoWithOverlay.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const VideoWithOverlay = ({ videoSrc, text }) => {
+const VideoWithOverlay = ({ videoSrc, text, imageSrc }) => {
   const [typedText, setTypedText] = useState("");
   const [subText, setSubText] = useState("");
+  const [isVideoEnded, setIsVideoEnded] = useState(false);
   const textRef = useRef();
+  const videoRef = useRef(null);
 
+  // Type out text and subtext
   useEffect(() => {
     const textToType = text.videoOverlay.split("");
     const textToTypeSubtext = text.videoOverlaySubtext.split("");
@@ -14,20 +17,20 @@ const VideoWithOverlay = ({ videoSrc, text }) => {
 
     const intervalId = setInterval(() => {
       index++;
-
       if (index < textToType.length) {
         setTypedText((prevText) => prevText + textToType[index]);
       } else {
         clearInterval(intervalId);
 
-        const subTextToType = text.videoOverlaySubtext.split("");
         let subIndex = 0;
         setSubText(textToTypeSubtext[0]);
 
         const subIntervalId = setInterval(() => {
           subIndex++;
-          if (subIndex < subTextToType.length) {
-            setSubText((prevSubText) => prevSubText + subTextToType[subIndex]);
+          if (subIndex < textToTypeSubtext.length) {
+            setSubText(
+              (prevSubText) => prevSubText + textToTypeSubtext[subIndex]
+            );
           } else {
             clearInterval(subIntervalId);
           }
@@ -40,12 +43,39 @@ const VideoWithOverlay = ({ videoSrc, text }) => {
     };
   }, [text.videoOverlay, text.videoOverlaySubtext]);
 
+  // Listen for the video ending
+  useEffect(() => {
+    const handleVideoEnd = () => setIsVideoEnded(true);
+
+    if (videoRef.current) {
+      videoRef.current.addEventListener("ended", handleVideoEnd);
+
+      return () => {
+        videoRef.current.removeEventListener("ended", handleVideoEnd);
+      };
+    }
+  }, [videoRef]);
+
   return (
     <div className="relative w-full h-full bg-gray-500">
-      <video autoPlay muted playsInline className="w-full h-full">
-        <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      {!isVideoEnded ? (
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          playsInline
+          className="w-full h-full"
+        >
+          <source src={videoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        <img
+          src={imageSrc}
+          alt="High-resolution image"
+          className="w-full h-full"
+        />
+      )}
       <div
         ref={textRef}
         className="absolute w-full h-full flex flex-col justify-center items-center top-1/2 left-1/2 tracking-wide transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 p-4 rounded text-white whitespace-pre-wrap"
